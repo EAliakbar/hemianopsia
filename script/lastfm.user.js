57be70f9ca7230c261d47d86ce715550b4d76018
Amir Ghassemi Nasr <Ameretat.Reith@gmail.com>
2013-08-07 22:28:31 +0430

    last.fm: seperate rules by URL

--- script/lastfm.user.js
+++ script/lastfm.user.js
@@ -6,32 +6,36 @@
 // @include     http://www.lastfm.tld/*
 // @require     https://raw.github.com/reith/hemianopsia/master/core.js
 // @downloadURL https://raw.github.com/reith/hemianopsia/master/script/lastfm.user.js
+// @grant       none
+// @version     1.1
 // @run-at      document-end
 // ==/UserScript==
 
 (function() {
 	if (window.top != window.self) return;
-
 	var manifest = [
 		{
+			path_re: /^\/user\/[^\/]+\/journal/,
 			selector: '#content div.journals div.journalBody div.bbcode',
 			comment: 'journal text'
 		}, {
-			selector: 'div.shoutboxContainer ul.shouts li.clearit p',
-			root_selector: 'div#content > div.leftCol > div.leftColWrapper > div.deferred',
-			live: true,
-			comment: 'shoutbox in user and group profile'
-		}, {
+			path_re: /shoutbox$/,
 			selector: '#content div.fullWidth div.skylineRight div.skyWrap div#shoutbox ul#shoutList.shouts li.clearit p',
 			comment: 'paged shoutbox'
 		}, {
-			selector: 'div.shoutboxContainer ul.shouts li.clearit p',
-			root_selector: 'body.r-artist > div.page-wrapper > article.content > div.artist-social > section > div.deferred',
-			live: true,
-			comment: 'shoutbox in artist page'
-		}, {
-			selector: '#content div.journals div#shoutbox ul#shoutList.shouts li.clearit p',
+			path_re: /^\/user\/[^\/]+\/journal/,
 			comment: 'journal comments'
+		}, {
+			path_re: [
+				/^\/music\/[^\/]+\//,
+				/^\/user\/[^\/]+$/,
+				/^\/group\/[^\/]+$/,
+				/^\/music\/[^\/]+$/
+			],
+			root_selector: 'div.deferred',
+			selector: 'div.shoutboxContainer ul.shouts li p',
+			live: true,
+			comment: 'in-page shoutbox'
 		}
 	];
 
