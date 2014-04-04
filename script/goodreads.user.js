b74178ed6e828e8b32e8e10034d16910c5758b48
Amir Ghassemi Nasr <Ameretat.Reith@gmail.com>
2014-04-04 23:21:42 +0430

    added some rules for textareas

--- script/goodreads.user.js
+++ script/goodreads.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name        www.goodreads.com bidi
+// @name        Goodreads.com bidi
 // @namespace   https://github.com/reith/hemianopsia
 // @description This script rearranges right-to-left written texts
 // @include     https://www.goodreads.com/*
 // @require     https://raw.github.com/reith/hemianopsia/master/core.js
 // @downloadURL https://raw.github.com/reith/hemianopsia/master/script/goodreads.user.js
 // @grant       none
-// @version     0.1
+// @version     0.2
 // @run-at      document-end
 // ==/UserScript==
 
@@ -30,6 +30,25 @@
 			path_re: /^\/author\/show/,
 			selector: 'div.quoteText',
 			comment: 'quotes from author',
+		}, {
+			path_re: [
+				/^\/topic\/show/,
+				/^\/review\/show/
+			],
+			selector: 'textarea#comment_body_usertext',
+			input: true,
+			comment: 'user comments on topic and review pages'
+		}, {
+			path_re: /^\/group\/show/,
+			selector: '#topInfo',
+			comment: 'group info'
+		}, {
+			path_re: /^\/book\/show/,
+			root_selector: 'div.mainContentFloat',
+			selector: 'div.reviewFooter textarea',
+			live: true,
+			input: true,
+			comment: 'comment textbox on book page'
 		}
 	];
 
