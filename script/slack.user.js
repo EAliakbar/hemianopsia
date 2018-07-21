c664afe81c2a498507aca8ddbe5f933e171c5f1d
Amir Ghassemi Nasr <amirghasemi@rayanai.ir>
2018-07-21 12:53:21 +0430

    updated for new slack class names

--- script/slack.user.js
+++ script/slack.user.js
@@ -6,14 +6,14 @@
 // @require     https://raw.github.com/reith/hemianopsia/master/core.js
 // @downloadURL https://raw.github.com/reith/hemianopsia/master/script/slack.user.js
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @run-at      document-end
 // ==/UserScript==
 
 (function() {
 	if (window.top != window.self) return;
 	var manifest = [
-		{
+		{ // DEPRECATED
 			root_selector: '#msgs_div',
 			live: true,
 			selector: [
@@ -21,6 +21,14 @@
 				'div.comment'
 			],
 			comment: 'messages and their comments'
+		},
+		{
+			root_selector: '#messages_container',
+			live: true,
+			selector: [
+				'span.c-message__body',
+			],
+			comment: 'New messages'
 		}
 	];
 
